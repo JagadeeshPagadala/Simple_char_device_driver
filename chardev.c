9d00ae217c024e9277d2c4cda18224f0b28c43b4
JagadeeshPagadala <jagadeesh.pgdl@gmail.com>
2016-04-16 19:04:58 +0530

    Code improvement.

--- chardev.c
+++ chardev.c
@@ -8,14 +8,10 @@
 #include <linux/cdev.h>
 #include <linux/slab.h>
 
-
-#define BUFFERS 10000
-
 static dev_t major;
 static struct cdev char_dev;
+//static struct cdev char_dev1;
 static struct class *class;
-
-
 
 int my_open(struct inode* inode, struct file* file)
 {
@@ -31,19 +27,7 @@ int my_close(struct inode* inode, struct file* file)
 
 ssize_t my_write(struct file* file, const  char __user *data,size_t num, loff_t* offset)
 {
-	int* buffer[BUFFERS];
-	int i=0;
 	printk(KERN_EMERG"its write");
-	for(i=0;i<BUFFERS;i++)
-	{
-		buffer[i]=kmalloc(1024,GFP_KERNEL);
-		if(!buffer[i])
-		{
-			printk(KERN_EMERG"\n kmalloc failed in %d allocation \n",i);
-			return -ENOMEM;
-		}
-	}
-	printk(KERN_INFO"\n kamlloc succes %d \n",i);
 	return num;
 }
 
@@ -61,18 +45,23 @@ struct file_operations fileops=
 	.write=my_write,
 	.read=my_read,
 };
+
 static int kmalloc_init(void)
 {
+
 	printk(KERN_EMERG"init module");
-	if(alloc_chrdev_region(&major,0,2,"jk")<0)
+
+	if(alloc_chrdev_region(&major,0,2,"testdevice")<0)
 	{
 		return -1;
 	}
-	if((class=class_create(THIS_MODULE,"malloc_class"))==NULL)
+
+	if((class=class_create(THIS_MODULE,"test_class"))==NULL)
 	{
 		unregister_chrdev_region(major,2);
 		return -1;
 	}
+
 	cdev_init(&char_dev,&fileops);
 	if(cdev_add(&char_dev,major,1)==-1)
 	{
@@ -81,19 +70,42 @@ static int kmalloc_init(void)
 		unregister_chrdev_region(major,2);
 		return -1;
 	}
-	if(device_create(class,NULL,major,NULL,"jk0")==NULL)
+
+	if(device_create(class,NULL,major+0,NULL,"testdevice0")==NULL)
 	{
 		device_destroy(class,major);
 		class_destroy(class);
 		unregister_chrdev_region(major,2);
 		return -1;
 	}
+#if 0
+	/*FIXME: 
+		Enable this block when a logic is found to register two devs 
+		under a same major number*/
+	cdev_init(&char_dev1,&fileops);
+	if(cdev_add(&char_dev1,major+1,1)==-1)
+	{
+		device_destroy(class,major);
+		class_destroy(class);
+		unregister_chrdev_region(major,2);
+		return -1;
+	}
+
+	if(device_create(class,NULL,major+1,NULL,"testdevice1")==NULL)
+	{
+		device_destroy(class,major);
+		class_destroy(class);
+		unregister_chrdev_region(major,2);
+		return -1;
+	}
+#endif
 	return 0;
 }
 
 static void kmalloc_exit(void)
 {
 	cdev_del(&char_dev);
+//	cdev_del(&char_dev1);
 	device_destroy(class,major);
 	class_destroy(class);
 	unregister_chrdev_region(major,2);
@@ -103,6 +115,6 @@ static void kmalloc_exit(void)
 
 module_init(kmalloc_init);
 module_exit(kmalloc_exit);
-MODULE_AUTHOR("JAGADEESH PAGADALA");
+MODULE_AUTHOR("Jagadeesh Pagadala");
 MODULE_DESCRIPTION("A simple character device");
 MODULE_LICENSE("GPL");
